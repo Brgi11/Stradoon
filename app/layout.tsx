9fc0e670b1e4efef1b4af692b11f8c6ab3dde0fb
Stradoon <info@stradoon.com>
2026-04-27 20:47:29 +0200

    Remove mobile margins from main content

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,7 +46,7 @@ export default function RootLayout({
         </a>
         <LanguageProvider>
           <Header />
-          <main id="main-content" className="px-2 sm:px-0">{children}</main>
+          <main id="main-content">{children}</main>
           <Footer />
         </LanguageProvider>
       </body>
